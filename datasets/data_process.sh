2fb149ddac135511e4b8031222498fab3800c8df
Olafyii <804093559@qq.com>
2019-08-02 05:02:57 +0800

    modified datasets/data_process.sh

--- datasets/data_process.sh
+++ datasets/data_process.sh
@@ -1,3 +1,3 @@
-th video2frame.lua --classes WavingFlag
-python collectinfo.py --classes WavingFlag
-th generateMeta.lua --classes WavingFlag
+th video2frame.lua --classes $1
+python collectinfo.py --classes $1
+th generateMeta.lua --classes $1
